8861a29cc31d4482c1862f7f82d344b5699253e3
Lisbaldy <lisba7474@gmail.com>
2020-04-13 21:30:19 -0300

    Lectura de articulos de texto automatizada

--- src/components/ArticleContent.jsx
+++ src/components/ArticleContent.jsx
@@ -24,15 +24,32 @@ const ArticleContent = () => {
         if(id === item.id) // Se usa estrictamente igual (===) ya que id fue parseado.
         { 
             currentArticle = item;
+            return true; //Solo es necesario para eliminar el warning de consola.
         }
-        return null; //Solo es necesario para eliminar el warning de consola.
+        return false;
     })
+
+    let arrayOfStrings = currentArticle.text.split("&");
     
     return (
         <React.Fragment>
             <article className="articleContent">
-                <h2>{currentArticle.title}</h2>
-                <p>{currentArticle.text}</p>
+                <div className="articleContentDiv">
+                    <h1 className="articleContentH1">{currentArticle.title}</h1>
+                    {
+                        arrayOfStrings.map(item => 
+                            (
+                                (item.indexOf("<p>") !== -1) ?
+                                <p className="articleContentP">{item.trim().slice(3,-4)}</p>
+                                : (item.indexOf("<h2>") !== -1) ?
+                                <h2 className="articleContentH2">{item.trim().slice(4,-5)}</h2>
+                                : (item.indexOf("<h3>") !== -1) ?
+                                <h3 className="articleContentH3">{item.trim().slice(4,-5)}</h3>
+                                : null
+                            )
+                        )
+                    }
+                </div>
             </article>
             <Footer />
         </React.Fragment>
