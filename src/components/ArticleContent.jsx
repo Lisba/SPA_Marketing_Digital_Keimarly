634b9837f965484fcc7ef4c45a94d7018f06281a
Lisbaldy <lisba7474@gmail.com>
2020-04-19 00:38:38 -0300

    funcionalidad blog agregada

--- src/components/ArticleContent.jsx
+++ src/components/ArticleContent.jsx
@@ -1,54 +1,40 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Footer from './Footer';
-import articles from '../textArticles'
+import React from 'react'
+import Footer from './Footer'
+import MDXPost1 from '../posts/post1.mdx'
+import MDXPost2 from '../posts/post2.mdx'
+import MDXPost3 from '../posts/post3.mdx'
+import { useParams } from 'react-router-dom'
 
 const ArticleContent = () => {
 
     let {id} = useParams();
-    id = parseInt(id); //Solo es necesario para eliminar el warning de consola.
 
-    let currentArticle;
+    const [post, setPost] = React.useState('');
 
-    // Para mayor rendimiento ya que se usa break al encontrar el item buscado:
-    // for(let i=0; i<articles.length; i++)
-    // {
-    //     if(articles[i].id == id)
-    //     {
-    //         currentArticle = articles[i];
-    //         break;
-    //     }
-    // }
+    // fetch('https://github.com/Lisba/Sistema-de-control-de-cachorros/blob/master/README.md')
+    // .then(response => response.json())
+    // .then(data => console.log(data))
 
-    articles.map(item => {
-        if(id === item.id) // Se usa estrictamente igual (===) ya que id fue parseado.
-        { 
-            currentArticle = item;
-            return true; //Solo es necesario para eliminar el warning de consola.
+    React.useEffect( () => {
+        switch(id)
+        {
+            case '1':
+                setPost(MDXPost1)
+                break;
+            case '2':
+                setPost(MDXPost2)
+                break;
+            default:
+                setPost(MDXPost3)
+                break;
         }
-        return false;
-    })
+    }, [id])
 
-    let arrayOfStrings = currentArticle.text.split("&");
-    
     return (
         <React.Fragment>
             <article className="articleContent">
                 <div className="articleContentDiv">
-                    <h1 className="articleContentH1">{currentArticle.title}</h1>
-                    {
-                        arrayOfStrings.map(item => 
-                            (
-                                (item.indexOf("<p>") !== -1) ?
-                                <p className="articleContentP">{item.trim().slice(3,-4)}</p>
-                                : (item.indexOf("<h2>") !== -1) ?
-                                <h2 className="articleContentH2">{item.trim().slice(4,-5)}</h2>
-                                : (item.indexOf("<h3>") !== -1) ?
-                                <h3 className="articleContentH3">{item.trim().slice(4,-5)}</h3>
-                                : null
-                            )
-                        )
-                    }
+                    {post}
                 </div>
             </article>
             <Footer />
